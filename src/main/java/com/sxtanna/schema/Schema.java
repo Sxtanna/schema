5d2036e05028d013ff09ecc7ccba575455235540
Sxtanna <ranald.taylor@gmail.com>
2020-09-21 08:51:25 -0400

    added documentation to exposed classes

--- src/main/java/com/sxtanna/schema/Schema.java
+++ src/main/java/com/sxtanna/schema/Schema.java
@@ -3,20 +3,28 @@ package com.sxtanna.schema;
 import com.sxtanna.schema.document.SchemaDocument;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.InputStreamReader;
 import java.io.Reader;
 
+/**
+ * The entry point for creating {@link SchemaDocument} instances.
+ */
 public final class Schema
 {
 
+	/**
+	 * Load a document from the provided reader using the provider type
+	 *
+	 * @param reader where to read the document from
+	 * @param type   which type to read the document as
+	 *
+	 * @return The new document representing the contents from the reader
+	 *
+	 * @apiNote Makes zero assumptions about the state of the provided reader, it is up to the caller to ensure readiness
+	 *          and to close the reader.
+	 */
 	public static SchemaDocument load(@NotNull final Reader reader, @NotNull final SchemaDocument.Type type)
 	{
 		return type.get(reader);
-	}
-
-	public static SchemaDocument loadFromResource(@NotNull final String path, @NotNull final SchemaDocument.Type type)
-	{
-		return load(new InputStreamReader(Schema.class.getResourceAsStream(path)), type);
 	}
 
 }
--- src/main/java/com/sxtanna/schema/document/SchemaDocument.java
+++ src/main/java/com/sxtanna/schema/document/SchemaDocument.java
@@ -10,31 +10,78 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 
+/**
+ * A document that holds data in various formats, accessible through paths delimited by periods.
+ */
 public interface SchemaDocument
 {
 
+	/**
+	 * Attempt to retrieve an instance of {@link T} from the provided path
+	 *
+	 * @param path  period delimited path to the object
+	 * @param clazz type of the object
+	 *
+	 * @return An optional containing the object at this path
+	 */
 	<T> Optional<T> get(@NotNull final String path,
 						@NotNull final Class<T> clazz);
 
 
+	/**
+	 * Attempt to retrieve a list of {@link T} objects from the provided path
+	 *
+	 * @param path  period delimited path to the list
+	 * @param clazz type of the objects in the list
+	 *
+	 * @return An optional containing the list of objects at this path
+	 */
 	<T> Optional<List<T>> getList(@NotNull final String path,
 								  @NotNull final Class<T> clazz);
 
 
+	/**
+	 * Attempt to retrieve a map of {@link K} to {@link V} objects from the provided path
+	 *
+	 * @param path   period delimited path to the map
+	 * @param clazzK type of the keys in the map
+	 * @param clazzV type of the values in the map
+	 *
+	 * @return An optional containing the map at this path
+	 */
 	<K, V> Optional<Map<K, V>> getMap(@NotNull final String path,
 									  @NotNull final Class<K> clazzK,
 									  @NotNull final Class<V> clazzV);
 
 
+	/**
+	 * Updates the value at the provided path with the provided value
+	 *
+	 * @param path period delimited path
+	 * @param data value to insert into the document
+	 *
+	 * @apiNote This method does not create parent nodes on a path
+	 */
 	<T> void set(@NotNull final String path,
 				 @Nullable final T data);
 
 
+	/**
+	 * Save this document to the provided writer
+	 *
+	 * @param writer the writer to consume this document in text form
+	 */
 	void save(@NotNull final Writer writer);
 
 
+	/**
+	 * The "type" or "format" of a specific schema document.
+	 */
 	enum Type
 	{
+		/**
+		 * A document using JSON.
+		 */
 		JSON(SchemaDocumentJson::new);
 
 
@@ -46,6 +93,13 @@ public interface SchemaDocument
 		}
 
 
+		/**
+		 * Parse a {@link SchemaDocument} from a the provided reader using this type
+		 *
+		 * @param reader where to read the contents of the document from
+		 *
+		 * @return a document representing the contents of the reader
+		 */
 		public SchemaDocument get(@NotNull final Reader reader)
 		{
 			return function.apply(reader);
