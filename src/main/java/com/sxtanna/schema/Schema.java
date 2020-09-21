f5e83d7db15043964df5bfc254e64e160143c53b
Sxtanna <ranald.taylor@gmail.com>
2020-09-21 07:55:17 -0400

    added schema loading from resource

--- src/main/java/com/sxtanna/schema/Schema.java
+++ src/main/java/com/sxtanna/schema/Schema.java
@@ -3,6 +3,7 @@ package com.sxtanna.schema;
 import com.sxtanna.schema.format.SchemaFormat;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
 import java.util.Map;
@@ -42,6 +43,11 @@ public final class Schema implements SchemaFormat
 	public static Schema load(@NotNull final Reader reader, @NotNull final SchemaFormat.Type type)
 	{
 		return new Schema(type.get(reader));
+	}
+
+	public static Schema loadFromResource(@NotNull final String path, @NotNull final SchemaFormat.Type type)
+	{
+		return load(new InputStreamReader(Schema.class.getResourceAsStream(path)), type);
 	}
 
 }
